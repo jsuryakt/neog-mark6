9838b949010beb45a7ce9703e11fb7a49b317541
Jayasurya <jsuryakt@gmail.com>
2021-09-18 11:32:15 +0530

    Add stylesheet

--- main.js
+++ main.js
@@ -11,20 +11,17 @@ function translateToMinion() {
   var input = document.querySelector("#input-area").value;
   console.log(input);
   if (input == "") {
+    output.innerText = "";
     alert("Enter something to translate");
   } else {
-    var api = "https://api.funtranslations.com/translate/yoda.json";
+    var api = "https://api.funtranslations.com/translate/minion.json";
     var url = api + "?text=" + input;
     fetch(url)
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        if (json.error.message != "") {
-          errorHandler(json.error.message);
-        } else {
           var translated = json.contents.translated;
           output.innerText = translated;
-        }
       })
       .catch(errorHandler);
   }
