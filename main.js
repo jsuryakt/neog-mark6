92767ab9f70764fb80309ff9b0394e49f2235c77
Jayasurya <jsuryakt@gmail.com>
2021-09-18 14:38:06 +0530

    Add audio speech API Handle too many request error 429 Hide or display audio button

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 var button = document.querySelector("#translate-btn");
 var output = document.querySelector("#output-area");
+var translated = output.value;
+var audiobtn = document.querySelector("#audio");
 
 button.addEventListener("click", translateToMinion);
 
 function errorHandler(error) {
-  alert("Error: " + error);
+  if (
+    error ==
+    "TypeError: Cannot read properties of undefined (reading 'translated')"
+  ) {
+    alert("Please wait for an hour and try later!");
+  } else {
+    alert("Error: " + error);
+  }
 }
 
-function translateToMinion() {
+function translateToMinion(event) {
+  event.preventDefault();
   var input = document.querySelector("#input-area").value;
-  console.log(input);
   if (input == "") {
     output.innerText = "";
+    audiobtn.style.display = "none";
     alert("Enter something to translate");
   } else {
     var api = "https://api.funtranslations.com/translate/minion.json";
@@ -19,10 +29,43 @@ function translateToMinion() {
     fetch(url)
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
-          var translated = json.contents.translated;
-          output.innerText = translated;
+        translated = json.contents.translated;
+        output.innerText = translated;
+        audiobtn.style.display = "block";
       })
       .catch(errorHandler);
   }
+}
+
+var synth = window.speechSynthesis;
+var pitchValue = 2; //0.1 - 2
+var rateValue = 2; //0.1 - 10
+var voiceIndex = 2; // 0-4
+
+function speak() {
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+  if (translated !== "") {
+    var utterThis = new SpeechSynthesisUtterance(translated);
+    utterThis.onend = function (event) {
+      console.log("SpeechSynthesisUtterance.onend");
+    };
+    utterThis.onerror = function (event) {
+      console.error("SpeechSynthesisUtterance.onerror");
+    };
+
+    utterThis.voice = synth.getVoices()[voiceIndex];
+    utterThis.pitch = pitchValue;
+    utterThis.rate = rateValue;
+    synth.speak(utterThis);
+  }
+}
+
+audiobtn.addEventListener("click", listenAudio);
+
+function listenAudio(event) {
+  event.preventDefault();
+  speak();
 }
